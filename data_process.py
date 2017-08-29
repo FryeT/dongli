1b59848c3c33b40515e8028c4f6eb762f35ddf99
FryeT <fengleitan@hotmail.com>
2017-08-29 17:47:49 +0800

    change modify.py to modify all txt file into utf-8

--- data_process.py
+++ data_process.py
@@ -4,13 +4,13 @@ import sys
 
 cols = ['时间','振次','轴位移','力','应力','变形',
         '侧位移','围压','孔压','反压','体变','小体变']
-def data_process(f):
-    df = pd.read_table(f, encoding='gb2312')
+def data_process(fname):
+    df = pd.read_table(fname)
     df.columns = cols
     df.drop(['侧位移','体变','小体变','应力'],1,inplace=True)
     df.dropna(inplace=True)
-    df.to_csv(sys.argv[1][:-4]+'.csv',index=False)
+    df.to_csv(fname[:-4]+'.csv',index=False)
 
 if __name__ == '__main__':
-    for file in sys.argv:
+    for file in sys.argv[1:]:
         data_process(file)
--- modify.py
+++ modify.py
@@ -1,17 +1,28 @@
 #-*- coding:utf-8 -*-
 
-def modify(f, d):
-    with open(f,'r+',encoding='gb2312') as f:
+def modify(fname, d):
+    try:
+        f = open(fname, encoding='utf-8')
+        s = f.read()
+    except:
+        f = open(fname, encoding='gb2312')
         s = f.read().replace(d,'\t')
+        f.close()
         s = s.lstrip()
-        f.seek(0)
+        s = s.encode('utf-8')
+        f = open(fname,'wb')
         f.write(s)
+        f.close()
+        print('modified')
+    else:
+        f.close()
+        print('have been modified')
 
 if __name__ == '__main__':
     from sys import argv
     import os
-    for f in argv[1:]:
-        if os.path.isfile(f):
-            modify(f,',')
+    for fname in argv[1:]:
+        if os.path.isfile(fname):
+            modify(fname,',')
 
 
