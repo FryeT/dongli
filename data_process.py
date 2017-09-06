5af4718845af741c011a0ee9585937ed6f9f6f4e
FryeT <fengleitan@hotmail.com>
2017-09-06 11:33:43 +0800

    mm

--- data_process.py
+++ data_process.py
@@ -4,10 +4,11 @@ import sys
 
 cols = ['时间','振次','轴位移','力','应力','变形',
         '侧位移','围压','孔压','反压','体变','小体变']
+drop_cols = ['侧位移','体变','小体变','应力']
 def data_process(fname):
     df = pd.read_table(fname)
     df.columns = cols
-    df.drop(['侧位移','体变','小体变','应力'],1,inplace=True)
+    df.drop(drop_cols,1,inplace=True)
     df.dropna(inplace=True)
     df.to_csv(fname[:-4]+'.csv',index=False)
 
